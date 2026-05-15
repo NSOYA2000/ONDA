e779975cf88c9ca2bc9c571fb5e77a140b429197
nan.zhou.ibm <nan.zhou.ibm@extern.faw-vw.com>
2026-05-16 05:26:34 +0800

    fix: switch BrowserRouter to HashRouter to fix URL redirect and refresh issues on GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Home from './pages/Home';
 import ChooseFormat from './pages/ChooseFormat';
@@ -9,7 +9,7 @@ import SupportedFormats from './pages/SupportedFormats';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
@@ -21,6 +21,6 @@ export default function App() {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
